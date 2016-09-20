4820d6e17347e3196c2a81818cec6e0801dcf5c7
Shaquille Johnson <sjohns25@dltsandbox.library.nd.edu>
2016-09-20 11:58:39 -0400

    latest file changes

--- script/build-ruby.sh
+++ script/build-ruby.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 set -xe
+mkdir -p ~/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
+echo '%_topdir %(echo $HOME)/rpmbuild' > ~/.rpmmacros
+find ~/rpm\-specs -name 'ruby.spec' -exec cp {} ~/rpmbuild/SPECS  \;
+# yum-builddep ~/rpmbuild/SPECS/ruby.spec
 
 RUBY_X_Y_Z_VERSION=$(grep "%define \+rubyver" $HOME/rpmbuild/SPECS/ruby.spec | awk '{print $3}')
 RUBY_X_Y_VERSION=$(echo $RUBY_X_Y_Z_VERSION | sed -e 's@\.[0-9]$@@')
